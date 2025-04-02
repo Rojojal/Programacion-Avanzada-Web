316ae96bfea82f592f47aa5a2d52e315310754d0
jonafide <jesquivel40315@ufide.ac.cr>
2025-04-01 19:41:57 -0600

    crear API Clima

---
+++ PAP.Mvc/Controllers/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PAP.Services;
+
+namespace PAP.Mvc.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController(WeatherService weatherService) : ControllerBase
+    {
+        private readonly WeatherService _weatherService = weatherService;
+
+        [HttpGet("current")]
+        public async Task<IActionResult> GetCurrentWeather([FromQuery] double latitude, [FromQuery] double longitude)
+        {
+            var weather = await _weatherService.GetCurrentWeatherAsync(latitude, longitude);
+
+            if (weather is null)
+                return NotFound("Datos del clima no disponibles.");
+
+            return Ok(weather);
+        }
+    }
+}
